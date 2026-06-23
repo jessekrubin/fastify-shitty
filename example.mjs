a63427c27d2b3764cf58680d43391d0f7922c246
jessekrubin <jessekrubin@gmail.com>
2026-06-23 14:20:31 -0700

    updates

--- example.mjs
+++ example.mjs
@@ -9,7 +9,9 @@ fastify.get("/", {}, (request, reply) => {
   reply.send({ hello: "world" });
 });
 
-fastify.listen({ port: 3000, host: "0.0.0.0" }).catch((err) => {
+try {
+  fastify.listen({ port: 3000, host: "0.0.0.0" });
+} catch (err) {
   fastify.log.error(err);
   process.exit(1);
-});
+}
