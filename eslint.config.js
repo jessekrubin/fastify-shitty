b53f6ce7e6decd82a6df349a9191d82b3b2377b7
jessekrubin <jessekrubin@gmail.com>
2026-06-30 14:25:11 -0700

    ok

--- eslint.config.js
+++ eslint.config.js
@@ -3,9 +3,12 @@ import { jsse } from "@jsse/eslint-config";
 export default jsse({
   ignores: ["**/dev"],
   debug: false,
-  typescript: { strict: true, tsconfig: ["tsconfig.json", "tsconfig.eslint.json"] },
+  typescript: {
+    strict: true,
+    tsconfig: ["tsconfig.json", "tsconfig.eslint.json"],
+  },
   reportUnusedDisableDirectives: true,
-  off: [] ,
+  off: [],
   prettier: true,
   stylistic: false,
 });
