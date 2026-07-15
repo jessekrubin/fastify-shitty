eae538833b79659a7d5f8ad0963e1e001110bc35
jesse <jessekrubin@gmail.com>
2026-07-15 07:37:25 -0700

    update (#88)
    * update
    
    * fmt

--- src/fastify-shitty.ts
+++ src/fastify-shitty.ts
@@ -1,7 +1,19 @@
 import type { FastifyPluginAsync, FastifyPluginCallback } from "fastify";
 import fp from "fastify-plugin";
 
-export type FastifyShittyOptions = { odds?: number; delay?: boolean };
+export type FastifyShittyOptions = {
+  /**
+   * The odds of a request failing; a number between 0 and 1. Defaults to 0.2 (20%).
+   * @default 0.2
+   */
+  odds?: number;
+  /**
+   * Whether to delay the response by a random amount of time (up to 1 second). Defaults to true.
+   * @default true
+   */
+  delay?: boolean;
+};
+
 export const pluginName = "@jsse/fastify-shitty";
 const DEFAULT_OPTS: Required<FastifyShittyOptions> = { odds: 0.2, delay: true };
 
@@ -11,21 +23,21 @@ export const fastifyShittyCallback: FastifyPluginCallback<
   const shittyLog = fastify.log.child({ plugin: pluginName });
 
   const { odds, delay } = { ...DEFAULT_OPTS, ...opts };
+  if (odds < 0 || odds > 1) {
+    throw new Error(
+      "[@jsse/fastify-shitty] odds must be a number between 0 and 1",
+    );
+  }
   fastify.addHook("onRequest", (_request, _reply, done) => {
     const rand = Math.random();
     const err =
       rand > odds ? undefined : new Error("Houston we have a problem...");
     if (delay) {
       const delay = Math.floor(rand * 1000);
-      // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
       shittyLog.info(`Delaying response by ${delay}ms`);
-      setTimeout(
-        () => {
-          done(err);
-        },
-
-        delay,
-      );
+      setTimeout(() => {
+        done(err);
+      }, delay);
     } else {
       done(err);
     }
