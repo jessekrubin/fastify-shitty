2e8b7f44b66159b444a2e587b7f027450e22b1f0
jessekrubin <jessekrubin@gmail.com>
2026-07-15 07:30:59 -0700

    update

--- src/fastify-shitty.test.ts
+++ src/fastify-shitty.test.ts
@@ -1,5 +1,5 @@
 import Fastify from "fastify";
-import { expect, test } from "vitest";
+import { describe, expect, test } from "vitest";
 import { FastifyShitty } from "./fastify-shitty.js";
 
 test("fastify-shitty", async () => {
@@ -21,4 +21,22 @@ test("fastify-shitty", async () => {
   };
   await Promise.all(Array.from({ length: nreqs }, () => mkreq()));
   expect(nerrors).toBeGreaterThan(0);
+});
+
+describe("bad options", () => {
+  test("odds < 0", async () => {
+    const fastify = Fastify();
+    const promise = fastify.register(FastifyShitty, { odds: -1 });
+    await expect(promise).rejects.toThrow(
+      "[@jsse/fastify-shitty] odds must be a number between 0 and 1",
+    );
+  });
+
+  test("odds > 1", async () => {
+    const fastify = Fastify();
+    const promise = fastify.register(FastifyShitty, { odds: 2 });
+    await expect(promise).rejects.toThrow(
+      "[@jsse/fastify-shitty] odds must be a number between 0 and 1",
+    );
+  });
 });
