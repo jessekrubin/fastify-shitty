bd908fb0a79a5366afd5d71762d9c52bea5daf77
jessekrubin <jessekrubin@gmail.com>
2026-08-04 17:29:08 -0700

    done

--- example.mjs
+++ example.mjs
@@ -5,7 +5,7 @@ import FastifyShitty from "./dist/fastify-shitty.js";
 const fastify = Fastify({ logger: true });
 fastify.register(FastifyShitty, { odds: 0.3, delay: true });
 
-fastify.get("/", {}, (request, reply) => {
+fastify.get("/", {}, (_request, reply) => {
   reply.send({ hello: "world" });
 });
 
